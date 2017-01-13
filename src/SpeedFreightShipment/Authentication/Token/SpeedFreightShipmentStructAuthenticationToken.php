15bf3d8c92989bf7dac62755f8893463eb76d10a
Ben Wilson <ben@mainlight.com>
2017-01-13 15:50:47 -0500

    Fixes authentication failure
    Extending with SpeedFreightShipmentWsdlClass caused additional SOAP headers to be sent and authentication to fail.

--- src/SpeedFreightShipment/Authentication/Token/SpeedFreightShipmentStructAuthenticationToken.php
+++ src/SpeedFreightShipment/Authentication/Token/SpeedFreightShipmentStructAuthenticationToken.php
@@ -1,23 +1,14 @@
 <?php
 /**
- * File for class SpeedFreightShipmentStructAuthenticationToken
- * @package SpeedFreightShipment
- * @subpackage Structs
- * @author WsdlToPhp Team <contact@wsdltophp.com>
- * @version 20150429-01
- * @date 2017-01-04
+ * AuthenticationToken
+ *
+ * @author        Ben Wilson
+ * @link          https://github.com/benwilson
+ * @package       SpeedFreightShipment
+ * @subpackage    Structs
+ *
  */
-/**
- * This class stands for SpeedFreightShipmentStructAuthenticationToken originally named AuthenticationToken
- * Meta informations extracted from the WSDL
- * - from schema : var/wsdltophp.com/storage/wsdls/35165-07413ab26bb23f8884bc8617c9846509/wsdl.xml
- * @package SpeedFreightShipment
- * @subpackage Structs
- * @author WsdlToPhp Team <contact@wsdltophp.com>
- * @version 20150429-01
- * @date 2017-01-04
- */
-class SpeedFreightShipmentStructAuthenticationToken extends SpeedFreightShipmentWsdlClass
+class SpeedFreightShipmentStructAuthenticationToken
 {
     /**
      * The loginId
@@ -49,16 +40,27 @@ class SpeedFreightShipmentStructAuthenticationToken extends SpeedFreightShipment
     public $accountNumber;
     /**
      * Constructor method for AuthenticationToken
-     * @see parent::__construct()
-     * @param string $_loginId
+     * @param string|array $_loginId loginId string or array of 'loginId', 'password', 'licenseKey', 'accountNumber'
      * @param string $_password
      * @param string $_licenseKey
      * @param string $_accountNumber
      * @return SpeedFreightShipmentStructAuthenticationToken
      */
-    public function __construct($_loginId = NULL,$_password = NULL,$_licenseKey = NULL,$_accountNumber = NULL)
-    {
-        parent::__construct(array('loginId'=>$_loginId,'password'=>$_password,'licenseKey'=>$_licenseKey,'accountNumber'=>$_accountNumber),false);
+    public function __construct($_loginId = NULL,$_password = NULL,$_licenseKey = NULL,$_accountNumber = NULL) {
+	    $properties = array(
+			    'loginId'       => $_loginId,
+			    'password'      => $_password,
+			    'licenseKey'    => $_licenseKey,
+			    'accountNumber' => $_accountNumber );
+	    if ( is_array( $_loginId ) ) {
+		    $properties = array_merge( $properties, $_loginId );
+	    }
+	    foreach ( $properties as $k => $v ) {
+		    $setMethod = 'set' . ucfirst( $k );
+		    if ( method_exists( $this, $setMethod ) ) {
+			    $this->$setMethod( $v );
+		    }
+	    }
     }
     /**
      * Get loginId value
@@ -127,25 +129,5 @@ class SpeedFreightShipmentStructAuthenticationToken extends SpeedFreightShipment
     public function setAccountNumber($_accountNumber)
     {
         return ($this->accountNumber = $_accountNumber);
-    }
-    /**
-     * Method called when an object has been exported with var_export() functions
-     * It allows to return an object instantiated with the values
-     * @see SpeedFreightShipmentWsdlClass::__set_state()
-     * @uses SpeedFreightShipmentWsdlClass::__set_state()
-     * @param array $_array the exported values
-     * @return SpeedFreightShipmentStructAuthenticationToken
-     */
-    public static function __set_state(array $_array,$_className = __CLASS__)
-    {
-        return parent::__set_state($_array,$_className);
-    }
-    /**
-     * Method returning the class name
-     * @return string __CLASS__
-     */
-    public function __toString()
-    {
-        return __CLASS__;
     }
 }
